df336ac6c0126cdb8befcb2194cb670d02650e7c
Guntuboina Sankirtana <59257797+gvsankirtana@users.noreply.github.com>
2022-02-26 20:27:41 +0300

    new changes

--- Notify-app/backend/index.js
+++ Notify-app/backend/index.js
@@ -1,11 +1,11 @@
-const conenectToMongo = require('./db');
+const conenectToMongo = require("./db");
 conenectToMongo();
-const express = require('express')
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require("express");
+const app = express();
+const port = 3000;
+//Available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/auth',require('./routes/notes'))
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
