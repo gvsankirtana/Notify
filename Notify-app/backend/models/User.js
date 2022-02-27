11655a322ce7d38fdea82f03f9aded7b1ad076ed
Guntuboina Sankirtana <59257797+gvsankirtana@users.noreply.github.com>
2022-02-27 19:01:47 +0300

    new changes

--- Notify-app/backend/models/User.js
+++ Notify-app/backend/models/User.js
@@ -8,7 +8,6 @@ const UserSchema = new Schema({
     email:{
         type: String,
         required: true,
-        unique: true
     },
     password:{
         type:String,
@@ -19,4 +18,6 @@ const UserSchema = new Schema({
         default: Date.now
     },
   })
-  module.exports = mongoose.model('user',UserSchema);
+  const User = mongoose.model('user',UserSchema);
+  User.createIndexes();
+  module.exports = User;
--- Notify-app/backend/routes/auth.js
+++ Notify-app/backend/routes/auth.js
@@ -4,15 +4,21 @@ const User = require('../models/User');
 const router = express.Router();
 //Create a user using : POST "/api/auth/". Doesnt require Auth
 router.post('/',[
-    body('name').isLength({ min: 3 }),
-    body('email').isEmail(),
-    body('password').isLength({ min: 5 }),
+    body('name','Enter a valid name').isLength({ min: 3 }),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password must be atleast 5 characters').isLength({ min: 5 }),
 ],
 (req,res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send(req.body);
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err=>{console.log(err)
+    res.json({error:'please enter a unique value for email',message:err.message})})
 })
 module.exports = router
