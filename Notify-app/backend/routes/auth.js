4d5549becce8bd8f133c5b4341a554764cc5ab8e
Guntuboina Sankirtana <59257797+gvsankirtana@users.noreply.github.com>
2022-02-27 13:56:06 +0300

    new changes

--- Notify-app/backend/routes/auth.js
+++ Notify-app/backend/routes/auth.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = express.Router();
 //Create a user using : POST "/api/auth/". Doesnt require Auth
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
+router.post('/',[
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+],
+(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     res.send(req.body);
 })
 module.exports = router
